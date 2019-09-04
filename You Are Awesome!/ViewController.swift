7c8aad96dabe05cd7904681b2b01b9c06d0dab5c
Daniel Yang <yangafc@bc.edu>
2019-09-03 20:21:41 -0400

    2.6 completed

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -9,23 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     @IBOutlet weak var messageLabel: UILabel!
     // Code below executes when the app first loads
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
 
     @IBAction func showMessagePressed(_ sender: UIButton) {
-        messageLabel.text = "You Are Awesome!"
-        messageLabel.textColor = UIColor.red
+        if messageLabel.text == "You Are Awesome!" {
+            messageLabel.text = "You Are Great!"
+        } else if messageLabel.text == "You Are Great!" {
+            messageLabel.text = "You Are Amazing!"
+        } else {
+            messageLabel.text = "You Are Awesome!"
+        }
     }
-    
-    @IBAction func showAnotherMessagePressed(_ sender: UIButton) {
-        messageLabel.text = "You are Great!"
-        messageLabel.textColor = UIColor.blue
-    }
+
     
 }
 
